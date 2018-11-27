b3edbea8ab9fff70cd80860ccc016974bd324ce6
TimothyFell <timothy.fell846@gmail.com>
2018-11-27 14:30:26 -0700

    finished merchant record endpoints

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,13 +21,63 @@ describe "Merchants API" do
 
     get "/api/v1/merchants/#{id}"
 
+    expect(response).to be_successful
+
     merchant = JSON.parse(response.body)
 
     expect(merchant["id"]).to eq(id)
   end
 
-  it "can create a new merchant" do
+  it "can find a merchant by their id" do
+    id = create(:merchant).id
 
+    get "/api/v1/merchants/find?id=#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can find a merchant by their name" do
+    name = create(:merchant).name
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["name"]).to eq(name)
+  end
+
+  it "can find a merchant by their created_at datetime" do
+    merchant = create(:merchant, created_at: '2012-03-27 14:53:59 UTC')
+    created_at = merchant.created_at
+    id = merchant.id
+
+    get "/api/v1/merchants/find?created_at=#{created_at}"
+
+    expect(response).to be_successful
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(json_merchant["id"]).to eq(id)
+  end
+
+  it "can find a merchant by their updated_at datetime" do
+    merchant = create(:merchant, updated_at: '2012-03-27 14:53:59 UTC')
+    updated_at = merchant.updated_at
+    id = merchant.id
+
+    get "/api/v1/merchants/find?updated_at=#{updated_at}"
+
+    expect(response).to be_successful
+
+    json_merchant = JSON.parse(response.body)
+
+    expect(json_merchant["id"]).to eq(id)
   end
 
 end
