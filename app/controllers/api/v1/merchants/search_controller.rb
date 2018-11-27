a13d5d33dedc76d02ae6d79a784ca4df7e303f83
TimothyFell <timothy.fell846@gmail.com>
2018-11-27 15:23:30 -0700

    add rspec and factory to production to debug heroku not working

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -5,7 +5,13 @@ class Api::V1::Merchants::SearchController < ApplicationController
   end
 
   def show
-    render json: Merchant.look_up(params)
+    render json: Merchant.find_by(look_up_params)
+  end
+
+  private
+
+  def look_up_params
+    params.permit('id','name','created_at', 'updated_at')
   end
 
 end
